d67e0f809d12eb53932849f5bf8c028f96436378
Kevin Ma <kevinkf.ma@gmail.com>
2019-06-04 23:20:31 -0600

    Hooked up slack credentials to firestore

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,15 @@
-import { App } from '@slack/bolt';
+import { App, AuthorizeResult } from '@slack/bolt';
 import { BlockButtonAction, ButtonAction } from '@slack/bolt/dist/types'
 import { PollSession } from './models/PollSession';
 import { PollSessionFactory, MockPollSessionFactory, LivePollSessionFactory } from './helpers/PollSessionFactory';
-
-import { createClient } from '@google/maps'
 import { LivePollOptionFactory } from './helpers/PollOptionFactory';
+
+import { Firestore } from '@google-cloud/firestore';
+import { createClient } from '@google/maps'
+
+const db = new Firestore({
+    projectId: 'luncheon-roulette'
+});
 
 const mapClient = createClient({
     key: process.env.MAPS_API_KEY,
@@ -16,27 +21,43 @@ const sessions : Map<string, PollSession> = new Map<string, PollSession>();
 //TODO: Use DI
 const sessionFactory : PollSessionFactory = new LivePollSessionFactory(new LivePollOptionFactory(mapClient));
 
+const authorizeFn = async({teamId}) : Promise<AuthorizeResult> => {
+    const teams = await db.collection('slackauth').get();
+
+    for (let i = 0; i < teams.size; i++) {
+        const teamData = teams.docs[0].data();
+        if (teamData.teamId = teamId) {
+            return {
+                botId: teamData.botId,
+                botUserId: teamData.botUserId,
+                botToken: teamData.botToken,
+                userToken: teamData.userToken
+            } as AuthorizeResult
+        }
+    };
+    
+    throw new Error('No matching authorization');
+}
+
 const app = new App(
     {
-        token: process.env.LUNCH_BOT_TOKEN,
-        signingSecret: process.env.LUNCHEON_ROULETTE_SIGNING_SECRET,
-        botId: "lunchbot",
-        botUserId: "lunchbot"
+        authorize: authorizeFn,
+        signingSecret: process.env.LUNCHEON_ROULETTE_SIGNING_SECRET
     }
 );
 
-app.event("app_mention", async ({payload, body}) => {
+app.event("app_mention", async ({payload, body, context}) => {
     const id : string = body.team_id + payload.channel;  
     const session : PollSession = await sessionFactory.build(id, "The wheel has been spun!\n*Where should we go for lunch?*");   
-
+    context;
     // Response types aren't strongly typed
     const messageResult: any = await app.client.chat.postMessage(
         {
-            "token": process.env.LUNCH_BOT_TOKEN,
-            "channel": payload.channel,
-            "text": "This text does not matter :/",
-            "as_user": true,
-            "blocks": session.render()
+            token: context.botToken,
+            channel: payload.channel,
+            text: "This text does not matter :/",
+            as_user: true,
+            blocks: session.render()
         }
     );
 
@@ -45,7 +66,7 @@ app.event("app_mention", async ({payload, body}) => {
     }
 });
 
-app.action("vote_button", async ({ack, action, body}) => {
+app.action("vote_button", async ({ack, action, body, context}) => {
     ack();
 
     const button: BlockButtonAction = body as BlockButtonAction;
@@ -54,8 +75,8 @@ app.action("vote_button", async ({ack, action, body}) => {
     const session = sessions.get(button.message.ts);
 
     const profileResult: any = await app.client.users.profile.get({
-        "token": process.env.LUNCH_USER_TOKEN,
-        "user": body.user.id  
+        token: context.userToken,
+        user: body.user.id  
     });
 
     if (profileResult.ok) {
@@ -66,12 +87,12 @@ app.action("vote_button", async ({ack, action, body}) => {
 
         app.client.chat.update(
             {
-                "token": process.env.LUNCH_BOT_TOKEN,
-                "channel": button.channel.id, 
-                "text": 'This text does not matter',
-                "ts": button.message.ts,          
-                "as_user": true,                         
-                "blocks": session.render()
+                token: context.botToken,
+                channel: button.channel.id, 
+                text: 'This text does not matter',
+                ts: button.message.ts,          
+                as_user: true,                         
+                blocks: session.render()
             }
         );
     }
