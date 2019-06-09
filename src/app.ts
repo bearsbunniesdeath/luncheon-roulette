c9c8a82fc19a51635ba2c92e80328bf7845270ce
Kevin Ma <kevinkf.ma@gmail.com>
2019-06-09 11:22:35 -0600

    Update add to slack button

--- src/app.ts
+++ src/app.ts
@@ -132,7 +132,7 @@ app.error((error) => {
 // OAuth workflow to install the app //////////////////////////
 
 expressApp.get('/', (req, res) => {
-    res.send('<h1>Luncheon Roulette</h1><a href="https://slack.com/oauth/authorize?scope=bot&client_id=76198394647.628689207987"><img alt=""Add to Slack"" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x" /></a>');
+    res.send('<h1>Luncheon Roulette</h1><a href="https://slack.com/oauth/authorize?client_id=76198394647.628689207987&scope=bot,chat:write:bot,users.profile:read"><img alt="Add to Slack" height="40" width="139" src="https://platform.slack-edge.com/img/add_to_slack.png" srcset="https://platform.slack-edge.com/img/add_to_slack.png 1x, https://platform.slack-edge.com/img/add_to_slack@2x.png 2x"></a>');
 });
 
 expressApp.get('/slack/auth/result', (req, res) => {
